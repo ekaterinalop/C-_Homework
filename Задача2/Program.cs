2ae07628e2457980c1808c978b6e542579b8f4cd
Katya Lop <eaasadulina>
2022-08-12 12:00:17 +0300

    Max from 2 numbers

---
+++ Задача2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите число a=");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b=");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("max=");
+    Console.WriteLine(a);
+    
+}
+else
+{
+    Console.WriteLine("max=");
+    Console.WriteLine(b);
+}
