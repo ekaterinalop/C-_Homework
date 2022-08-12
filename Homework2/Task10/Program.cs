4a816f8e093f26a901076222582bd28caa2e1e8a
Katya Lop <eaasadulina>
2022-08-12 18:34:53 +0300

    homework2: task 10,13

---
+++ Homework2/Task10/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("введите трехзачное число:");
+string number = Console.ReadLine();
+Console.WriteLine(number[1]);
---
+++ Homework2/Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число от 1 до 7 соответствующее дню недели");
+int number =Console.ReadLine();
+  
+void Weekend ()
+[]
+        Console.WriteLine( number[2]);
+    
+
+
