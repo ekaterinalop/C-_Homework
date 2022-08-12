fc427c01076daa68293d863e54ca8779e60e7cec
Katya Lop <eaasadulina>
2022-08-12 12:11:54 +0300

    Task4, max from 3 numbers

---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("введите число a=");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b=");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число c=");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    if(a>c)
+    {
+        Console.WriteLine("max=");
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine("max=");
+        Console.WriteLine(c);
+    }  
+}
+else
+{
+    if (b>c)
+    {
+        Console.WriteLine("max=");
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine("max=");
+        Console.WriteLine(c);
+    }
+}
