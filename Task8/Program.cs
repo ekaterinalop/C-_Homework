991deb3ff53a362465cf65a87b89c8cc0d539860
Katya Lop <eaasadulina>
2022-08-12 16:08:57 +0300

    even numbers from 1 to N

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a <= n)
+{   
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+    a += 1;
+}
