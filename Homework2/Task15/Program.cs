ad98de3caa4d003b6eff64087fa4fb5a2e4321a5
Katya Lop <eaasadulina>
2022-08-12 18:57:05 +0300

    Weekend check

---
+++ Homework2/Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите цифру, соответствующую дню недели от 1 до 7");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void Weekend (int day)
+ {
+  if (day == 6)
+  {
+  Console.WriteLine("этот день выходной");
+  }
+  else if (day == 7) 
+  {
+    Console.WriteLine("этот день выходной");
+  }
+   else Console.WriteLine("этот день рабочий");
+}
+
+Weekend(day);
