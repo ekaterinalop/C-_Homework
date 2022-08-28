ead78195e469d968c2dd63e7241c441dca729646
Katya Lop <eaasadulina>
2022-08-28 18:24:24 +0300

    41,43

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите через запятую M чисел");
+        int qty=0;
+        string numbers =Console.ReadLine();
+        string[] array=numbers.Split (",");
+        int[]arrayInt=new int[array.Length];
+        for (int i = 0; i <array.Length; i++)
+        {
+             arrayInt[i]=int.Parse(array[i]);
+             if (arrayInt[i]>0)
+                {
+                    qty+=1;
+                }
+        }
+        Console.WriteLine (qty);       
+    }    
+}
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] lines=new int[2,2];
+
+        Console.WriteLine("Введите значение k1, для уравнения y = k1 * x + b1");
+        lines[0,0]=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение b1, для уравнения y = k1 * x + b1");
+        lines[0,1]=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите значение k2, для уравнения y = k2 * x + b2");
+        lines[1,0]=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение b2, для уравнения y = k2 * x + b2");
+        lines[1,1]=Convert.ToInt32(Console.ReadLine());
+        
+        int x = (lines[1,1]-lines[0,1])/(lines[0,0]-lines[1,0]);
+        //x=(b2-b1)/(k1-k2);
+        int y = x*(lines[0,0]-lines[0,1]);
+        //y=k1*x+b1;
+
+         Console.WriteLine ($"координаты точки пересечения прямых y={lines[0,0]}*x+ {lines[0,1]} и y={lines[1,0]}*x+ {lines[1,1]} это [{x};{y}]");  
+        }
+           
+    }    
